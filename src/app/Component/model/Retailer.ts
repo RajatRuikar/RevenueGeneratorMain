c2ac65444f791995979c42fb34fb8f54ed8e9f30
RajatRuikar <rajatruikar12@gmail.com>
2024-07-27 12:49:41 +0530

    some changes on customer

--- src/app/Component/model/Retailer.ts
+++ src/app/Component/model/Retailer.ts
@@ -1,15 +1,15 @@
-export class Retailer{
-    retailerId:number;
-	retailerName: String;
-	retailerContactNumber: String;
-	retailerEmail: String;
-	retailerAdditionalDetails: String;
+export class Retailer {
+    retailerId: number;
+    retailerName: String;
+    retailerContactNumber: String;
+    retailerEmail: String;
+    retailerAdditionalDetails: String;
 
-    constructor(){
-        this.retailerId=0;
-        this.retailerName="";
-        this.retailerContactNumber="";
-        this.retailerEmail="";
-        this.retailerAdditionalDetails="";
+    constructor() {
+        this.retailerId = 0;
+        this.retailerName = "";
+        this.retailerContactNumber = "";
+        this.retailerEmail = "";
+        this.retailerAdditionalDetails = "";
     }
 }
