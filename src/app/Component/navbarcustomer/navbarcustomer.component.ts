8ab908b8de49e13ce78532e3c7ed15ad89395e92
RajatRuikar <rajatruikar12@gmail.com>
2024-07-30 23:50:28 +0530

    new navbar for customer

---
+++ src/app/Component/navbarcustomer/navbarcustomer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbarcustomer',
+  templateUrl: './navbarcustomer.component.html',
+  styleUrl: './navbarcustomer.component.css'
+})
+export class NavbarcustomerComponent {
+
+}
